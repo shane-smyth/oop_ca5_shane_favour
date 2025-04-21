57dd8a0219754be495ee52262f36fac824174fd9
f4eriebambi <d00271879@student.dkit.ie>
2025-04-21 18:28:16 +0100

    - created addDirector method

--- src/main/java/MoviesDaoGui/DAOs/DirectorDaoInterface.java
+++ src/main/java/MoviesDaoGui/DAOs/DirectorDaoInterface.java
@@ -1,7 +1,9 @@
 package MoviesDaoGui.DAOs;
 
+import MoviesDaoGui.DTOs.Director;
 import MoviesDaoGui.Exceptions.DaoException;
 
 public interface DirectorDaoInterface {
     public String getDirectorNameById(int id) throws DaoException;
+    public Director addDirector(Director director) throws DaoException;
 }
